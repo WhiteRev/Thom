d700b0104310340c66d83b4ed75f999960bfda20
WhiteRev <stevelandauer@gmail.com>
2019-04-20 00:40:14 +0200

    troll

---
+++ Python/Steve_Main.py
@@ -0,0 +1,14 @@
+
+#consigne cree une fonction print_str() qui affiche son attribut.
+
+
+#fonction
+def calcul(variable3_int):#variable3_int attribut
+	print(variable3_int - 9)
+
+
+variable1_int=15
+variable2_int=66
+variable3_int=variable2_int - variable1_int
+variable_str=""
+calcul(variable3_int)#call fonction
